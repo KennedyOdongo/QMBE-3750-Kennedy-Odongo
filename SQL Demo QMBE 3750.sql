f57813e085180c245ba50c7ceb93571417c0a193
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-01-30 15:34:11 -0600

    adding sql data

--- SQL Demo QMBE 3750.sql
+++ SQL Demo QMBE 3750.sql
@@ -1,15 +1,11 @@
-USE sakila;
-show tables;
-
-SHOW TABLES;
-
 # Create a database with your name. This is where we will create and store tables
 USE Kennedy;
-
 SHOW TABLES;
 CREATE TABLE class ( students varchar (255),age int, height float);
 
+#Show tables available in a database
 SHOW TABLES;
+
 #DROP TABLE table_name;
 DROP TABLE class;
 
@@ -17,6 +13,11 @@ INSERT INTO class (students, age, height)
 values('Kennedy Odongo', '1000', '4.5');
 
 SELECT *
+FROM kennedy.class;
+
+## Create a new attribute using AS keyword
+
+SELECT students, height*2 AS height_squared
 FROM kennedy.class;
 
 
