d441b80007a8f132d114221a8700a45f15bfe4a3
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-01-30 14:38:50 -0600

    made changes on the sql file

--- SQL Demo QMBE 3750.sql
+++ SQL Demo QMBE 3750.sql
@@ -6,3 +6,19 @@ SHOW TABLES;
 # Create a database with your name. This is where we will create and store tables
 USE Kennedy;
 
+SHOW TABLES;
+CREATE TABLE class ( students varchar (255),age int, height float);
+
+SHOW TABLES;
+#DROP TABLE table_name;
+DROP TABLE class;
+
+INSERT INTO class (students, age, height)
+values('Kennedy Odongo', '1000', '4.5');
+
+SELECT *
+FROM kennedy.class;
+
+
+
+
