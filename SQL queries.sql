7a93093c4c29d442c79830571834c4dc2bbc58d7
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-02-01 16:28:58 -0600

    added SQL files

--- SQL queries.sql
+++ SQL queries.sql
@@ -10,8 +10,21 @@ FROM tasks;
 
 SELECT price, category
 FROM tasks
+WHERE category = 'DRM';
+
+SELECT price,category
+FROM tasks
 WHERE price >= 50
-GROUP BY category, price
+GROUP BY price,category
+HAVING ABS(price)>40
+ORDER BY price
+LIMIT 10;
+
+
+SELECT price,category
+FROM tasks
+WHERE price BETWEEN 10 AND 100
+GROUP BY price,category
 HAVING ABS(price)>40
 ORDER BY price
 LIMIT 10;
