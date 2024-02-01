2af61fc7f019a592b6a36a757b3b7980c4fb2195
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-02-01 15:13:53 -0600

    adding SQL code

--- SQL queries.sql
+++ SQL queries.sql
@@ -5,3 +5,13 @@ SHOW TABLES;
 SELECT *
 FROM tasks;
 
+SELECT price, taskid
+FROM tasks;
+
+SELECT price, category
+FROM tasks
+WHERE price >= 50
+GROUP BY category, price
+HAVING ABS(price)>40
+ORDER BY price
+LIMIT 10;
