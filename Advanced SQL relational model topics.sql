1f32ee362a37ccd18d3b37838329eb08c618e28c
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-02-13 16:34:07 -0600

    adding SQL code

---
+++ Advanced SQL relational model topics.sql
@@ -0,0 +1,21 @@
+-- In SQL, a view is a virtual table that contains data from one or more tables. 
+-- Views have rows and columns, similar to a real table, but they don't hold any data.
+
+-- In SQL, a table is a physical data storage unit that stores data in a structured format.
+-- A view is a virtual table that provides a simplified interface for accessing data from one or more tables.
+
+USE bits;
+
+SHOW TABLES;
+
+CREATE VIEW Hardware AS
+SELECT taskid, description, price
+FROM tasks
+WHERE category IN ('ACC','HAM','PRI','UPG');
+
+SELECT * FROM hardware;
+
+CREATE or REPLACE VIEW Hardware AS
+SELECT taskid, description, price
+FROM tasks
+WHERE category IN ('ACC','HAM','PRI','UPG','OTH');
