d73c46dd76177384899ebd7d88ad06c53352c10c
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-02-23 10:44:07 -0600

    adding SQL code

--- creating_database_Kennedy.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE kennedy_project;
--- reconnect to DBMS
-
--- Alternatively, right click any schema on the navigation bar and create a schema that way. 
-
--- or the model approach-> Complete way to do it-> Forward engineering
-
-
