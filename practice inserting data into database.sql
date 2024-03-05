4f4e4ee64fb20fe6fc05d18f8a4730606de12ed9
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-03-05 08:41:34 -0600

    adding SQL code

---
+++ practice inserting data into database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE insert_data;
+
+USE insert_data;
+
+-- Using basic insert statement (We have covered this already).
+
+-- Using a .csv file.
+
+-- using rottentomatoes.csv on Canvas course space.
